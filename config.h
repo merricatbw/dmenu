b2ecfcbdc00895e65695f9d7936a7fa17030a67d
merricatbw <zkerr4032@outlook.com>
2023-11-10 13:22:10 +0000

    Change colorscheme

--- config.h
+++ config.h
@@ -3,17 +3,17 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bh = 2;               /* add an defined amount of pixels to the bar height */
+static const int user_bh = 5;               /* add an defined amount of pixels to the bar height */
 
 static const char *fonts[] = {
-	"Iosevka Nerd Font:pixelsize=17:antialias=true:autohing=true"
+	"Iosevka Nerd Font:pixelsize=15:antialias=true:autohing=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#282828" },
-	[SchemeSel] = { "#282828", "#d79921" },
-	[SchemeOut] = { "#ebdbb2", "#8ec07c" },
+	[SchemeNorm] = { "#cecece", "#121212" },
+	[SchemeSel] = { "#121212", "#cecece" },
+	[SchemeOut] = { "#cecece", "#8ec07c" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
